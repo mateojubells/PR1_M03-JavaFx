4cd0e5852e81d9fa5477527246c45e3846eb9a25
Oscar Gomez <oscargomezsedas@gmail.com>
2023-12-18 16:31:48 +0100

    añadidas las funciones para mostrar la información de la las listas de gastos e ingresos

--- PR1_M03/src/main/java/com/example/pr1_m03/MainPageController.java
+++ PR1_M03/src/main/java/com/example/pr1_m03/MainPageController.java
@@ -1,6 +1,9 @@
 package com.example.pr1_m03;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,9 +15,8 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainPageController {
-
     public PieChart chrt_ingresos;
-    public PieChart chart_gastos;
+    public PieChart chrt_gastos;
     public Button btn_ingresos;
     public Button btn_gastos;
     public Label lbl_gastos;
@@ -25,6 +27,7 @@ public class MainPageController {
 
     /**
      * Función que abre una ventana con la página de añadir ingresos
+     *
      * @param actionEvent N/A
      * @throws IOException Excepción en caso de error
      */
@@ -43,19 +46,43 @@ public class MainPageController {
 
     /**
      * Función que abre una ventana con la página de añadir gastos
+     *
      * @param actionEvent N/A
      * @throws IOException Excepción en caso de error
      */
     public void goGastos(ActionEvent actionEvent) throws IOException {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("add-expense.fxml"));
-            root = fxmlLoader.load();
-            scene = new Scene(root);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("add-expense.fxml"));
+        root = fxmlLoader.load();
+        scene = new Scene(root);
 
-            Stage newStage = new Stage();
-            newStage.setScene(scene);
-            newStage.setTitle("Agregar Gasto");
+        Stage newStage = new Stage();
+        newStage.setScene(scene);
+        newStage.setTitle("Agregar Gasto");
 
-            newStage.show();
-            // Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        newStage.show();
+        // Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
     }
+
+    @FXML
+    private void initialize() {
+        ObservableList<PieChart.Data> ingresosChartData = FXCollections.observableArrayList();
+
+        chrt_ingresos.setData(ingresosChartData);
+        ObservableList<PieChart.Data> gastosChartData = FXCollections.observableArrayList(
+
+        );
+
+        chrt_gastos.setData(ingresosChartData);
+    }
+
+    private void addDataIngresosChart() {
+        ObservableList<PieChart.Data> ingresosChartData = chrt_ingresos.getData();
+        // TODO: Crear función para añadir los ingresos creados
+    }
+
+    private void addDataGastosChart() {
+        ObservableList<PieChart.Data> gastosChartData = chrt_gastos.getData();
+        // TODO: Crear funcion para añadir los gastos creados
+    }
+
 }
