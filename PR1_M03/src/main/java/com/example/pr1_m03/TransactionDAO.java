5c6394dc3f4d688f247551e1fa07657c94ef3911
Óscar Gómez <oscargomezsedas@gmail.com>
2024-05-05 18:21:38 +0200

    creada funcion para obtener todas las transacciones

--- PR1_M03/src/main/java/com/example/pr1_m03/TransactionDAO.java
+++ PR1_M03/src/main/java/com/example/pr1_m03/TransactionDAO.java
@@ -2,4 +2,5 @@ package com.example.pr1_m03;
 
 public interface TransactionDAO {
     void saveTransaction(Transaction transaction);
+    void getAllTransactions();
 }
--- PR1_M03/src/main/java/com/example/pr1_m03/TransactionDAOImpl.java
+++ PR1_M03/src/main/java/com/example/pr1_m03/TransactionDAOImpl.java
@@ -1,12 +1,8 @@
 package com.example.pr1_m03;
 
-import com.example.pr1_m03.Transaction;
 import com.jdbc.utilities.ConnectDB;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TransactionDAOImpl implements TransactionDAO {
 
@@ -37,6 +33,51 @@ public class TransactionDAOImpl implements TransactionDAO {
         } finally {
             try {
                 // Cerrar la declaración
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void getAllTransactions() {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            // Obtener la conexión
+            connection = ConnectDB.getInstance();
+
+            // Preparar la consulta para obtener todas las transacciones
+            String query = "SELECT * FROM transactions";
+            statement = connection.createStatement();
+
+            // Ejecutar la consulta y obtener el resultado
+            resultSet = statement.executeQuery(query);
+
+            // Procesar los resultados
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String category = resultSet.getString("category");
+                double amount = resultSet.getDouble("amount");
+                String description = resultSet.getString("description");
+                Date date = resultSet.getDate("date");
+
+                // Aquí puedes hacer lo que quieras con los datos de la transacción, como imprimirlos por ejemplo
+                System.out.println("ID: " + id + ", Category: " + category + ", Amount: " + amount + ", Description: " + description + ", Date: " + date);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            // Cerrar los recursos
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
                 if (statement != null) {
                     statement.close();
                 }
